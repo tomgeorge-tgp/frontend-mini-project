ee3bb340e50e18967da534d2db34c1b0dae79839
ABHIJITH2001-MEC <75483427+ABHIJITH2001-MEC@users.noreply.github.com>
2023-04-28 11:55:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,13 @@
-import React, { useState } from 'react';
-import './LoginPage.css'; // import a separate CSS file for the component
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-const LoginPage = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+import App from "./App";
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  };
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // handle login logic here
-  };
-
-  return (
-    <div className="login-page"> {/* add a class for the overall page styling */}
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input type="text" value={username} onChange={handleUsernameChange} />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" value={password} onChange={handlePasswordChange} />
-        </label>
-        <br />
-        <button type="submit">Login</button>
-      </form>
-    </div>
-  );
-};
-
-export default LoginPage;
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
